29f1338f901b38835661bf99cfc18d7934b73c20
Brian <mulindijrn@gmail.com>
2024-05-30 13:43:31 +0300

    Add item update and addition handlers
    - Implemented  function to update the item list with a modified item.
      - Maps through the current items and replaces the updated item based on its ID.
      - Sets the updated item list state using .
    - Implemented  function to add a new item to the item list.
      - Appends the new item to the current list of items.
      - Updates the item list state using .

--- src/components/ShoppingList.js
+++ src/components/ShoppingList.js
@@ -13,6 +13,21 @@ function ShoppingList() {
     .then((items) => setItems(items))
   }, []);
 
+  function handleUpdateItem(updatedItem) {
+    const updatedItems = items.map((item) => {
+      if (item.id === updatedItem.id) {
+        return updatedItem;
+      } else {
+        return item;
+      }
+    });
+    setItems(updatedItems);
+  }
+
+  function handleAddItem(newItem){
+    setItems([...items, newItem])
+  }
+
   function handleCategoryChange(category) {
     setSelectedCategory(category);
   }
@@ -25,14 +40,14 @@ function ShoppingList() {
 
   return (
     <div className="ShoppingList">
-      <ItemForm />
+      <ItemForm onAddItem = {handleAddItem}/>
       <Filter
         category={selectedCategory}
         onCategoryChange={handleCategoryChange}
       />
       <ul className="Items">
         {itemsToDisplay.map((item) => (
-          <Item key={item.id} item={item} />
+          <Item key={item.id} item={item} onUpdateItem={handleUpdateItem} />
         ))}
       </ul>
     </div>
